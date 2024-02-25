e7a7be07e9aad2be162ef47cc08ee40f4b90f190
A13x37 <145167791+A13x37@users.noreply.github.com>
2024-02-25 15:53:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-document.querySelector('#search').addEventListener("click", search);
-
+document.querySelector('.search').addEventListener("click", search)
 
 async function search() {
-    let pokemon = document.querySelector('#pokemonName').value;
+    let pokemon = document.querySelector('#pokemon').value;
     pokemon = pokemon.toLowerCase();
-    const apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    const response = await fetch(apiURL);
-    const data = await response.json();
-    
-    document.querySelector('.infoContainer').innerHTML = `
+    const nameURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const promise1 = await fetch(nameURL);
+    const idURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const promise2 = await fetch(idURL);
+
+    Promise.any([promise1, promise2])
+    .then((response) => response.json())
+    .then((data) => {
+        document.querySelector('.infoContainer').innerHTML = `
     <div>
-        <img id="pokeImg" src="${data.sprites.other["official-artwork"].front_default}">
+        <img class="pokeImg" src="${data.sprites.other["official-artwork"].front_default}">
     </div>
     <div class="pokemonDescription">
         <h1>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h1>
         <p>Pokemon ID: ${data.id}</p>
     </div>
     `;
+    });    
 }
